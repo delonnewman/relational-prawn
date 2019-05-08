64d37b6e206f4697142921bf84dacd9a953fb033
Delon Newman <delnewman@salud.unm.edu>
2019-05-08 16:11:50 -0600

    FIX: ensure that body is an array, and scoped Prawn::Document

--- lib/relational/writer/pdf.rb
+++ lib/relational/writer/pdf.rb
@@ -8,12 +8,12 @@ module Relational
         title = opts[:title] || (relation.meta && relation.meta.fetch(:title, nil))
         subtitle = opts[:subtitle] || (relation.meta && relation.meta.fetch(:subtitle, nil))
 
-        pdf = Prawn::Document.new(opts)
+        pdf = ::Prawn::Document.new(opts)
         pdf.text title, size: 16, style: :bold if title
         pdf.text subtitle, size: 10 if title
         pdf.text "\n" if title or subtitle
 
-        data = relation.body.map(&:values)
+        data = relation.body.map(&:values).to_a
         data_table = [relation.header.map(&:to_s)] + data
 
         pdf.table(data_table) do
